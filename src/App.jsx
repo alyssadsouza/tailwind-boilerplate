04a6dc044729101cc65fc64fb22d1adbdca1c96c
alyssadsouza <alyssa.dsouza63@gmail.com>
2022-09-02 16:27:52 -0400

    Make dark mode bg darker

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className={`${dark ? "dark" : ""}`}>
       <div
-        className={`App flex flex-col justify-center w-full min-h-screen text-gray-800 dark:bg-gray-800 dark:text-white`}
+        className={`App flex flex-col justify-center w-full min-h-screen text-gray-800 dark:bg-gray-900 dark:text-white`}
       >
         <Header dark={dark} setDark={setDark} />
         <h1 className="text-center w-full text-5xl font-bold">
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import Toggle from "./Toggle";
 
 function Header({ dark, setDark }) {
   return (
-    <div className="flex flex-row justify-between items-center w-full h-16 border-gray-100 dark:border-gray-700 border-b absolute top-0">
+    <div className="flex flex-row justify-between items-center w-full h-16 border-gray-100 dark:border-gray-800 border-b absolute top-0">
       <img src="./vite.svg" className="mx-4" />
       <div className="flex flex-row justify-between items-center w-full">
         <div className="mx-2">
@@ -23,13 +23,13 @@ function Header({ dark, setDark }) {
           <Toggle dark={dark} setDark={setDark} />
           <a
             href="#"
-            className="mx-2 border border-gray-200 dark:border-gray-600 dark:hover:border-indigo-500 p-2 rounded-lg text-gray-400 text-sm dark:hover:text-indigo-400 hover:text-indigo-600 hover:border-indigo-500 transition-all"
+            className="mx-2 border border-gray-200 dark:border-gray-700 dark:hover:border-indigo-500 p-2 rounded-lg text-gray-400 text-sm dark:hover:text-indigo-400 hover:text-indigo-600 hover:border-indigo-500 transition-all"
           >
             Log In
           </a>
           <a
             href="#"
-            className="mx-2 border border-gray-200 dark:border-gray-600 dark:hover:border-indigo-500 p-2 rounded-lg text-gray-400 text-sm dark:hover:text-indigo-400 hover:text-indigo-600 hover:border-indigo-500 transition-all"
+            className="mx-2 border border-gray-200 dark:border-gray-700 dark:hover:border-indigo-500 p-2 rounded-lg text-gray-400 text-sm dark:hover:text-indigo-400 hover:text-indigo-600 hover:border-indigo-500 transition-all"
           >
             Sign Up
           </a>
